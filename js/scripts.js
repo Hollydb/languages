8714b4f892102845e47a01fb31131e45e10a3f85
hd <Holly.DeBelser@gmail.com>
2019-06-07 12:53:39 -0700

    add javascript and jquery function to pizzamath

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   console.log("Jquery is loaded");
   $("#submitbtn").click(function(event){
-  event.preventDefault()
+  event.preventDefault();
     var name = $('input#name').val();
     var experience = parseInt($('select#experience').val());
     var why = parseInt($('select#why').val());
@@ -44,51 +44,33 @@ $(document).ready(function() {
       $("#result4").hide();
     }
 });
-$(document).ready(function() {
-  console.log("pizza time")
-$("#submitbtnpizza").click(function(event){
-  event.preventDefault()
+
+
+  $("button#btnpizza").click(function(event){
+  event.preventDefault();
     var priceten = parseInt($('input#ten').val());
     var pricetwelve = parseFloat($('input#twelve').val());
     var pricefourteen = parseFloat($('input#fourteen').val());
-    console.log ('ten button is working');
-    
-  //   var value10 = (priceten / 314.16).val());
-  //   var value12 = (pricetwelve / 452.39).val());
-  //   var value14 = (pricefourteen / 615.75).val());
-  // console.log (value 14);
-  // if (total <= 8){
-  //     $("#result1").show();
-  //     $("#result2").hide();
-  //     $("#result3").hide();
-  //     $("#result4").hide();
-  //     $("#missed").hide();
-  //   } else if (total >=9 && total<=12){
-  //     $("#result2").show();
-  //     $("#result1").hide();
-  //     $("#result3").hide();
-  //     $("#result4").hide();
-  //     $("#missed").hide();
-  //   } else if (total >=13 && total<=17){
-  //     $("#result3").show();
-  //     $("#result2").hide();
-  //     $("#result1").hide();
-  //     $("#result4").hide();
-  //     $("#missed").hide();
-  //   } else if (total >=18 && total<=20){
-  //     $("#result4").show();
-  //     $("#result2").hide();
-  //     $("#result3").hide();
-  //     $("#result1").hide();
-  //     $("#missed").hide();
-  //   } else {
-  //     $("#missed").show();
-  //     $("#result1").hide();
-  //     $("#result2").hide();
-  //     $("#result3").hide();
-  //     $("#result4").hide();
-  //   }
-});
+    var valueten = (priceten/314.16);
+    var valuetwelve = (pricetwelve/452.39);
+    var valuefourteen = (pricefourteen/615.75);
+    console.log (valueten);
+    if (valueten < valuetwelve || valueten < valuefourteen){
+      $("#result10").show();
+      $("#result12").hide();
+      $("#result14").hide();
+      $("#missed").hide();
+    } else if (valuetwelve < valueten || valuetwelve < valuefourteen){
+      $("#result12").show();
+      $("#result10").hide();
+      $("#result14").hide();
+      $("#missed").hide();
+    } if (valuefourteen < valuetwelve || valuefourteen < valuefen){
+      $("#result14").show();
+      $("#result12").hide();
+      $("#result10").hide();
+      $("#missed").hide();
+    }
 
 });
 });
