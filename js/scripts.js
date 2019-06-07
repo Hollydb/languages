2b109f3b2c744605f00541b1356d7122764e2199
hd <Holly.DeBelser@gmail.com>
2019-06-07 09:53:53 -0700

    add styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
-$(document).ready(function)) {
+$(document).ready(function() {
   console.log("Jquery is loaded");
-  
-  
+  $("#submitbtn").click(function(event){
+  event.preventDefault()
+
+    var experience = parseInt($('select#experience').val());
+    var why = parseInt($('select#why').val());
+    var how = parseInt($('select#how').val());
+    var background = parseInt($('select#background').val());
+    var work = parseInt($('select#work').val());
+    var total = experience + why + how + background + work;
+    if (total <= 8){
+      $("#result1").show();
+      $("#result2").hide();
+      $("#result3").hide();
+      $("#result4").hide();
+    } else if (total >=9 && total<=12){
+      $("#result2").show();
+      $("#result1").hide();
+      $("#result3").hide();
+      $("#result4").hide();
+    } else if (total >=13 && total<=17){
+      $("#result3").show();
+      $("#result2").hide();
+      $("#result1").hide();
+      $("#result4").hide();
+    } else {
+      $("#result4").show();
+      $("#result2").hide();
+      $("#result3").hide();
+      $("#result1").hide();
+    }
+});
 });
